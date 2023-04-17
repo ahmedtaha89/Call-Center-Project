fcc8b86bc84a9eb2b6b0a93b41a710fecce262a9
ahmedtaha89 <ahmed8952at@gmail.com>
2023-04-17 14:47:34 +0200

    First Commit in Project

---
+++ Call_Center.sql
@@ -0,0 +1,9 @@
+/*
+Project Name: Call Center
+Data Set: https://www.mediafire.com/file/em7bnwhmf31o73u/01_Call-Center-Dataset.xlsx/file?dkey=xyvbhmulasf&r=621
+*/
+
+
+create database Call_Center;
+use Call_Center;
+select * from Call_Data;
