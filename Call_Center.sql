34927b0c85bae0a26b0d3e68164f6db15a85467b
ahmedtaha89 <ahmed8952at@gmail.com>
2023-04-17 15:57:26 +0200

    Modified

--- Call_Center.sql
+++ Call_Center.sql
@@ -39,3 +39,52 @@ drop procedure  Answered_call;
 -- Execute Procedure to Retrieve Percent Answered Call
 exec  Answered_call;
 
+
+-- Topic
+select Topic , count(Topic) as 'Number Of Call Per Topic'  from Call_Data
+group by Topic
+order by 2 desc;
+
+
+-- Avg Speed Of Answer
+select ROUND(AVG([Speed of answer in seconds]),2) as 'Avrge Speed Of Answer In Seconds' from Call_Data;
+
+
+-- Number Of Call Time (AM , PM)
+select [Call Time] , count([Call Time]) from Call_Data
+group by [Call Time]
+order by 2 desc;
+
+
+select Resolved , count(Resolved) from Call_Data
+group by Resolved; 
+
+
+
+Create Procedure Avg_Resolved
+as 
+begin
+declare @total_Problem decimal;
+declare @Resolve decimal;
+declare @Not_Resolve decimal;
+
+set @total_Problem = (select CONVERT(decimal,count(Resolved)) from Call_Data
+group by Resolved);
+
+set @Resolve = (
+select  CONVERT(decimal,count(Resolved))  from Call_Data
+where  Resolved = 'Yes'
+group by Resolved);
+
+set @Not_Resolve = (
+select   CONVERT(decimal,count(Resolved))  from Call_Data
+where  Resolved = 'No'
+group by Resolved);
+
+select (@Resolve / @total_Problem) *100 as 'Percent Total Resolve Problems' ,
+(@Not_Resolve / @total_Problem) *100 as 'Percent Total Not Resolve Problems' 
+end
+
+drop Procedure Avg_Resolved
+
+exec Avg_Resolved;
